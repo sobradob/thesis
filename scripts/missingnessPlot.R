00f505de81f8ebe405786157f617b76870bb0ead
sobradob <sobradob@gmail.com>
2017-11-14 18:52:23 +0800

    updated missingness plot to 5 minute intervals

---
+++ scripts/missingnessPlot.R
@@ -0,0 +1,73 @@
+# missing days with padr
+library(dplyr)
+library(padr)
+library(ggplot2)
+library(tibbletime)
+# read data
+all<-readRDS("../../data/peter/myLocationHistory.rds")
+attr(all$time, "tzone") <- "Europe/Paris"
+
+# by hour
+mHour<- all %>%
+  select(time) %>%
+  as_tbl_time(index = time) %>%
+  time_filter(2000-01 ~ 2017-06) %>% 
+  thicken('hour') %>%
+  group_by(time_hour) %>%
+  summarise(measurements = n()) %>%
+  pad() %>%
+  fill_by_value(value = 0) %>%
+  thicken("day") %>%
+  mutate(
+    type = case_when(
+      measurements== 0 ~ 1,
+      measurements > 0 ~ 0)) %>%
+  group_by(time_hour_day) %>%
+  summarise(hoursMissing = sum(type),
+            measurements = sum(measurements))
+
+library(scales)
+missingDay<- ggplot(mHour, aes(x = time_hour_day, y=measurements, colour= hoursMissing))+
+  geom_point()+theme_tufte()+
+  xlab("")+
+  ylab("Measurements")+
+  ggtitle("Amount of daily measurements over time")+
+  scale_colour_gradient2(low = muted("green"), mid = "grey50",
+                         high = muted("red"), midpoint = 12, space = "Lab",
+                         na.value = "grey50", guide = "colourbar")+
+  labs(x = NULL, colour = "Missing Hours")
+
+ggsave(missingDay,filename = "img/missingdayPeter.png",device = "png",height = 6.5, units = "cm")
+
+# by 5 minute
+m5min<- all %>%
+  select(time) %>%
+  as_tbl_time(index = time) %>%
+  time_filter(2000-01 ~ 2017-06) %>% 
+  thicken('5 min') %>%
+  group_by(time_5_min) %>%
+  summarise(measurements = n()) %>%
+  pad() %>%
+  fill_by_value(value = 0) %>%
+  thicken("day") %>%
+  mutate(
+    type = case_when(
+      measurements== 0 ~ 1,
+      measurements > 0 ~ 0)) %>%
+  group_by(time_5_min_day) %>%
+  summarise(min5Missing = sum(type),
+            measurements = sum(measurements))
+
+
+missingDay5min<- ggplot(m5min, aes(x = time_5_min_day, y=measurements, colour= min5Missing))+
+  geom_point()+theme_tufte()+
+  xlab("")+
+  ylab("Measurements")+
+  ggtitle("Missingness in daily measurements over time")+
+  scale_colour_gradient2(low = muted("green"), mid = "grey50",
+                         high = muted("red"), midpoint = 144, space = "Lab",
+                         na.value = "grey50", guide = "colourbar")+
+  labs(x = NULL, colour = "Missing \n5 minute\n segments")+
+  theme(legend.position = c(0.3, 0.85), legend.direction = "horizontal")
+
+ggsave(missingDay5min,filename = "../img/missingdayPeter5min.png",device = "png",height = 6.5, units = "cm")
