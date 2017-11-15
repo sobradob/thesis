2734f007690a3572642bb16d66a77578048034fe
sobradob <sobradob@gmail.com>
2017-11-15 22:14:22 +0800

    Some work on plots

--- scripts/missingnessPlot.R
+++ scripts/missingnessPlot.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(tibbletime)
 library(ggthemes)
 # read data
-all<-readRDS("../../data/peter/myLocationHistory.rds")
+all<-readRDS("../../data/boaz/myLocationHistory.rds")
 attr(all$time, "tzone") <- "Europe/Paris"
 
 # by hour
@@ -83,19 +83,23 @@ m5minv2<- all %>%
   summarise(measurements = n()) %>%
   pad() %>%
   fill_by_value(value = 0)%>%
-  mutate(hour = as.POSIXct(
-    paste0("2014-01-22 ",
-           strftime(time_5_min,format = "%H:%M", tz = "Europe/Budapest"))))
-  
-ggplot(m5minv2, aes(x = hour, y=measurements))+
+  mutate(
+    hour = as.POSIXct(
+      paste0("2014-01-22 ",
+           strftime(time_5_min,format = "%H:%M", tz = "Europe/Budapest"))),
+    time2 = lubridate::force_tz(time_5_min, "Asia/Singapore"),
+    missing = case_when(
+      measurements== 0 ~ 1,
+      measurements > 0 ~ 0)
+      )
+
+m5minv2 %>%
+  as_tbl_time(index = time2)%>%
+  time_filter(2017-02-15 ~ 2017-02-15) %>%
+  ggplot( aes(x = hour, y=measurements, colour = missing))+
   geom_point()+theme_tufte()+
   xlab("")+
   ylab("Measurements")+
-  ggtitle("Missingness in daily measurements over time")+
-  scale_colour_gradient2(low = muted("green"), mid = "grey50",
-                         high = muted("red"), midpoint = 144, space = "Lab",
-                         na.value = "grey50", guide = "colourbar")+
-  labs(x = NULL, colour = "Missing \n5 minute\n segments")+
-  theme(legend.position = c(0.3, 0.85), legend.direction = "horizontal")
+  ggtitle("Missingness in daily measurements over time")
 
 ggsave(missingDay5min,filename = "../img/missingdayPeter5min.png",device = "png",height = 6.5, units = "cm")
