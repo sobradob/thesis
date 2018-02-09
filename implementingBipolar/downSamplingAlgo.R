0559c66db3362a2b9c3344da48bb3b3a86bd207a
Boaz Sobrado <sobradob@gmail.com>
2018-02-09 17:38:34 +0100

    reorganised folders

---
+++ implementingBipolar/downSamplingAlgo.R
@@ -0,0 +1,105 @@
+# down sampling palmius algo
+
+# preceded by the filtering algo
+
+# divide in 1 hour epochs
+library(padr)
+
+# if lattitude and longitude sd both less than 0.01km
+# then replace with mean
+deg2rad <- function(deg) {(deg * pi) / (180)}
+
+#something fishy,figure out what it is
+
+d2<- d %>% thicken('hour') %>% 
+  group_by(time_hour) %>%
+  summarise(sdLat = sd(lat , na.rm = T),
+            sdLon = sd(lon , na.rm = T),
+            meanLat = mean(lat),
+            meanLon = mean(lon)) %>%
+  mutate(sdLatkm = sdLat*110.574,
+         sdLonkm = 111.320*cos(deg2rad(meanLat))
+         ) %>% 
+  pad() %>% 
+  fill_by_value( "NA") %>% 
+  filter(sdLatkm < 0.01 & sdLonkm < 0.01)
+
+summary(d2$sdLatkm)
+
+# Latitude: 1 deg = 110.574 km
+# Longitude: 1 deg = 111.320*cos(latitude) km
+
+
+#alternative version using pythagoras and distance via haversine forumla
+#in meters
+sqrt(10^2+10^2)
+
+d3<- d %>% thicken('hour') %>% 
+  group_by(time_hour) %>%
+  summarise(distancesMeterSd = sd(distancesMeter , na.rm = T),
+            meanLat = mean(lat),
+            meanLon = mean(lon)) %>% 
+  pad() %>% 
+  fill_by_value( "NA") %>%
+  filter(distancesMeterSd < 14.14) %>%
+  select(time_hour,meanLat,meanLon)
+
+dF<- d %>% thicken('hour') %>%
+  left_join(d3) %>%
+  mutate(
+    lonF1 = case_when(
+      is.na(meanLon) == F ~ meanLon
+    ),
+    latF1 = case_when(
+      is.na(meanLat) == F ~ meanLat
+    )
+  )
+
+dF2<- dF %>% thicken(by = "time","5 min",colname= "time_5min") %>% 
+  group_by(time_5min) %>% 
+  summarise(lonF2 = median(lon),
+            latF2 = median(lat)
+            )
+  
+d.filt <- dF %>% thicken(by = "time","5 min",colname= "time_5min") %>% 
+  left_join(dF2, by = "time_5min") %>%
+  select(time, time_5min, lat,lon, accuracy, latF1,lonF1,latF2,lonF2) %>% 
+  mutate(
+    lonF = case_when(
+    is.na(lonF1) ==F ~ lonF1,
+    is.na(lonF1) == T ~lonF2),
+    latF = case_when(
+      is.na(lonF1 ) == F ~ latF1,
+      is.na(lonF1 ) == T ~ latF2
+    )
+  )
+
+#summerise and filter and plot and shit
+d.filt %>% 
+  select(time_5min, lonF,latF)%>%
+  distinct() %>%
+  as_tbl_time(time_5min) %>% 
+  filter_time(~ "2017-02-15") %>% 
+  leaflet() %>%
+  addTiles() %>%
+  addCircles(lng = ~lonF, lat = ~latF)%>%
+  addProviderTiles(providers$CartoDB.Positron)
+
+# rolling 5 min filter
+test <- d %>% as_tbl_time(time) %>% 
+  filter_time(~ "2017-02-15")
+
+for(i in 1: nrow(test)){
+  timePoint <- test$time[i]
+  latlon<- test %>% filter(time > timePoint - 60*2.5 &  time < timePoint + 60*2.5) %>%
+    select(time,lat,lon) %>%
+    summarise(lat = median(lat),
+              lon = median(lon))
+  test[i,c("lat","lon")] <- latlon
+}
+
+test %>% 
+leaflet() %>%
+  addTiles() %>%
+  addCircles(lng = ~lon, lat = ~lat)%>%
+  addProviderTiles(providers$CartoDB.Positron)
---
+++ implementingBipolar/filteringAlgo.R
@@ -0,0 +1,89 @@
+# Palmius filtering algorithm implemented in R.
+
+# set wd
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#read data
+data<- readRDS("../data/boaz/myLocationHistory09012018.rds")
+
+# load auxiliary functions
+source("../thesis/scripts/auxFuns.R")
+#filter down to a day
+
+dataExample <- select(data,time,lat,lon,accuracy) %>%
+  arrange(time) %>% 
+  as_tbl_time(time) %>% 
+  filter_time( ~ '2017') %>%
+  mutate( nextLon = shift.vec(lon,-1),
+          nextLat = shift.vec(lat,-1),
+          nextTimeSec = shift.vec(time,-1)-time,
+          index = 1:length(lat)
+)
+
+rm(data)
+
+d<- dataExample
+dPrimeSource<- 100
+dPrimeDest <- 1
+removed <- data.frame(lon = 1, lat = 1)
+
+while(dPrimeSource >= 10 & dPrimeDest < 10){
+  
+  #calculate distance between points
+  d$distancesMeter<- apply(d, 1, FUN = function(row) {
+    raster::pointDistance(c(as.numeric(as.character(row["lon"])),
+                            as.numeric(as.character(row["lat"]))),
+                          c(as.numeric(as.character(row["nextLon"])), as.numeric(as.character(row["nextLat"]))),
+                          lonlat = T) # Parameter 'lonlat' has to be TRUE!
+  })
+  
+  #add index and calculate speed between points
+  d <- mutate(d,
+              speedMeterSecond= distancesMeter/as.numeric(nextTimeSec),
+              index = 1:nrow(dataExample)
+  )
+  
+  # set speed parameter
+  maxSpeed <- 27.7 #equivalent to 100 kmh
+  
+  # extract source 
+  filteredSource <- d %>% 
+    filter(speedMeterSecond >= maxSpeed)
+  
+  # extract destination
+  filteredDestination <- d %>%
+    filter(index %in% as.numeric(filteredSource$index+1))
+  
+  # extract modes
+  
+  sourceMode<- filteredSource %>% 
+    count(lon, lat) %>%
+    slice(which.max(n))
+  
+  destMode<- filteredDestination %>% 
+    count(lon, lat) %>%
+    slice(which.max(n))
+  
+  if(sourceMode$n >= destMode$n & sourceMode$n >10){
+    d <<- filter(d, lon != sourceMode$lon & lat != sourceMode$lat)
+    removed <- rbind(removed, sourceMode[,c("lon","lat")])
+    } else if(destMode$n >= 10){
+    d <<- filter(d, lon != destMode$lon & lat != destMode$lat)
+    removed <- rbind(removed, sourceMode[,c("lon","lat")])
+  }
+  
+  dPrimeSource<- sourceMode$n
+  dPrimeDest <- destMode$n
+  
+}
+
+# visual exploration for particular dates
+
+exploreDates(df = d, Data_Start = "2017-02-22",Data_End = "2017-02-22")
+exploreDates(df = dataExample, Data_Start = "2017-02-22",Data_End = "2017-02-22")
+
+# view 
+
+dRem <- filter(dataExample,
+               lon %in% removed$lon & lat %in% removed$lat)
+exploreDates(df = dRem,Data_Start = "2017",Data_End = "2017")
