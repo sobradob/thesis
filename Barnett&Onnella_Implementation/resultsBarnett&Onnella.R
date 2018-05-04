f69c57a686939ecab7c9efd71dabed0904015f1a
Boaz Sobrado <sobradob@gmail.com>
2018-05-04 11:55:44 +0200

    continued clean up

--- Barnett&Onnella_Implementation/resultsBarnett&Onnella.R
+++ Barnett&Onnella_Implementation/resultsBarnett&Onnella.R
@@ -1,72 +1,82 @@
-# full Ian results take 2
+# Full results of Barnett & Onnella's Method
+# this section cannot be reproduced without the functions provided by the authors of the methods
+# please contact them for their scripts
 
-directory <- "/Users/boazsobrado/Desktop/Academic/Utrecht/year2/thesisFiles/harvard/Example"
+
+directory <- "/harvard/Example"
 setwd(directory)
 
-
-#from finite
-data <- fin %>% as_tbl_time(time) %>% filter_time(~"2017-03")%>% select(timestampMs,time,lat,lon,accuracy)
+# set directory to the one with Barnett & Onella's scripts 
+directory <- "/harvard/Example"
+setwd(directory)
 
 # Load needed functions
-source("/Users/boazsobrado/Desktop/Academic/Utrecht/year2/thesisFiles/thesis/scripts/downSample.R")
-source("/Users/boazsobrado/Desktop/Academic/Utrecht/year2/thesisFiles/thesis/scripts/pauseFlight.R")
+source("downSample.R")
+source("pauseFlight.R")
 file.sources = list.files(pattern="*.R",path = "gpsmobility/R/")
 setwd("gpsmobility/R/")
 sapply(file.sources,source,.GlobalEnv)
 
+# select the needed data from the final pre-processed data
 data <- fin %>% select(timestampMs,time,lat,lon,accuracy)
-downsampledIan <- downSampleMean(data,interval = '5 min') #downsamples and adds XY coordinates
+
+#downsamples and adds XY coordinates
+downsampledIan <- downSampleMean(data,interval = '5 min') 
 
 
 # remove data for results
+## remove 5 minute bits
+## this takes approximately an hour on a macbook pro
 
-# 5 minute bits
-
-Sys.time()
+# take the time periods
 d2<- downsampledIan %>%
   mutate( time = as.POSIXct(timestampMs, origin="1970-01-01")) %>% 
   pull(time)
 
+# create a remove index
 remove_ind <- which(as.character(d2) %in% remove_ind_5min[[2]])
 
-Sys.time()
+# removed the selected time periods
 downsampled5minRemoved<- downsampledIan
 downsampled5minRemoved[remove_ind,c("lon","lat","x_v","y_v")] <- NA
 downsampled5minRemoved[remove_ind,c("code")] <- 4
 
-#params
+# set parameters
 minpausedur=120
 minpausedist=50
-Sys.time()
-mobmatmiss<- pauseFlight(downsampled5minRemoved, r = sqrt(300), w = mean(data$accuracy)) # extracts pauses and flights using algo
-Sys.time()
-mobmatmiss <- mobmatmiss[-nrow(mobmatmiss),] #clean a bit
 
-# Guessing Pauses
-Sys.time()
+
+# aggregate into pauses and flights
+mobmatmiss<- pauseFlight(downsampled5minRemoved, r = sqrt(300), w = mean(data$accuracy)) 
+
+# clear the data: the last row is erroneous
+mobmatmiss <- mobmatmiss[-nrow(mobmatmiss),] 
+
+# Guess pauses 
 mobmat = GuessPause(mobmatmiss,mindur=minpausedur,r=minpausedist)
-Sys.time()
+
+# initialise parameters
+
 obj=InitializeParams(mobmat)
+
+# set spread parameters
 spread_pars=c(10,1)
 wtype <- "TL"
-Sys.time()
-out3 <- SimulateMobilityGaps(mobmat,obj,wtype,spread_pars) # it is returning NA's only for
 
+# calculate mobility gaps
+out3 <- SimulateMobilityGaps(mobmat,obj,wtype,spread_pars)
+
+# deaggregate from pauses & flights to 5 minute window points and calculate distance
 evalDf<- evalIan(out3,downsampledIan = downsampledIan)
 
-# 5 minute thing
-
-evalDf[remove_ind,] %>% summary() # median 5 meters mean 141
-
-evalDf %>% filter( timestampMs %in% (downsampledIan[remove_ind,"timestampMs"] %>% pull())) %>% 
+# inspect distance measures
+evalDf %>% filter( timestampMs %in% (downsampledIan[remove_ind,"timestampMs"] %>%
+                                       pull())) %>% 
   unique() %>%
   summary()
 
-1-(65/length(remove_ind))
 
-#distA is the deviance score
-
-#check within accuracy 
+# Inspect deviance measures and their relation to accuracy
 
 devianceIan <- data %>% thicken("5 min") %>% 
   mutate(time_5_min = as.numeric(time_5_min)) %>%
@@ -74,27 +84,15 @@ devianceIan <- data %>% thicken("5 min") %>%
   left_join(.,evalDf, by = c("time_5_min" = "timestampMs")) %>%
   mutate(distB = raster::pointDistance(p1 = as.matrix(cbind(x_orig,y_orig)), p2 = as.matrix(cbind(x,y)),lonlat = F ))
 
-devianceIan %>% summarise( mean( accuracy >= distA, na.rm = T)) # approx 86% within accuracy 
+# Calculate what percentage of values are within accuracy
+devianceIan %>% summarise( mean( accuracy >= distA, na.rm = T)) 
 
-plotIan <- as.data.frame(out3) %>% filter(t0 <= 1488408900) %>% mutate( dur = t1-t0)
-plotIan2 <- devianceIan %>% as_tbl_time(time) %>% filter_time(~"2017-03-01")
-ggplot(plotIan, aes(x = x0, y = y0))+
-  geom_point(aes(size = (dur)))+
-  geom_segment(aes(x = x0, y = y0, xend = x1, yend = y1))+
-  coord_fixed()+
-  theme_tufte()+
-  xlab("")+ylab("")+
-  geom_point( data = plotIan2 , aes(x = x_orig, y = y_orig), shape =1, colour = "blue")
-
-
-devianceIan %>%  summarise(mean(accuracy >= distB, na.rm = T)) #86 percent of values are within accuracy 
-
-summary(devianceIan) # mean 343 median 8.21
+# Calculate average deviance measures
+summary(devianceIan) 
 
 
 # 1 hour thing
 
-# TO DO: SEPARATE INTO BEFORE MARCH, MARCH AND MARCH.
 
 # march
 removedHours<- remove_ind_hour[[2]]
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 # Missing data in GPS measurements
 
-This is the research archive necessary to make my thesis reproducible.
+This is the research archive necessary to make my thesis reproducible. The abstact of my thesis can be read below:
+
+>   Personal mobility, or how people move in their environment, is associated with a vast range of behavioural traits and outcomes, such as socioeconomic status, personality and mental health. The widespread adoption of location-sensor equipped smartphones has generated a wealth of objective personal mobility data. Nonetheless, smartphone collected personal mobility data has remained underutilised in behavioural research, partly due to the practical difficulties associated with obtaining the data and partly because of the methodological complexity associated with analysing it. Recent changes in European regulation have made it easier for researchers to obtain this data, but the methodological difficulties remain. The difficulty lies in that smartphone location data is irregularly sampled, sparse and often inaccurate.  This results in a high proportion of missing data and significant noise. In this paper we present a method called Personal Map Matched Imputation (PPMI) to deal with missing data and noise in smartphone location logs. The main innovation of PPMI is that it creates a personalised spatial map for each individual based on all the available data. In doing so PPMI leverages the regularity of human mobility in order to smoothen noisy measurments and impute missing data values. By simulating missing periods in real data we find that a simple implementation of PPMI performs as well as existing methods for short (5 minute) missing intervals and substantially better for longer (1 day) missing intervals. When imputing a subset of real  missing data where travel logs are available as a reference points, we find that PPMI performs substantially better than existing models.
+
+## Contents
 
 This archive contains:
 
@@ -12,14 +16,20 @@ This archive does not contain:
 1. Sensitive personal data, including smartphone location logs.
 2. Scripts written by others but nonetheless used in preparing the thesis, such as the MATLAB scripts of Palmius & the functions prepared by Barnett & Onnella. 
 
-The files are:
 
-[Correspondance](\correspondance): emails exchanged about the state of the thesis at different points.
-[Palmius Implementation](\PalmiusImplementation): Implementing the method of Palmius and getting subsequent results.
-[PPMI](\PPMI): PPMI implementation and objective results. 
-[scripts](\scripts): 
-[Thesis Manuscript](\thesis_manuscript): the files necessary to produce the text.
-[Thesis Report](\thesis_report): the files necessary to reproduce the thesis report submitted in 2017
+## Structure
 
 Files are generally structured to contain a main script, a folder of functions and a folder of scripts. The main script and the functions are the ones necessary to reproduce all results. The folder of scripts contains exploratory scripts written to wrangle data, test sub-hypotheses and contain research dead-ends that did not lead to desired results. What did work was prepared in the form of functions within the functions folders. 
+
+## Organisation 
+
+The files in this repository are:
+
+1. Correspondance: emails exchanged about the state of the thesis at different points.
+2. Palmius Implementation: Implementing the method of Palmius and getting subsequent results.
+3. PPMI: PPMI implementation and objective results. 
+4. Scripts: used to extract features, create images, etc. 
+5. Thesis Manuscript: the files necessary to produce the text.
+6. Thesis Report: the files necessary to reproduce the thesis report submitted in 2017
+
 
